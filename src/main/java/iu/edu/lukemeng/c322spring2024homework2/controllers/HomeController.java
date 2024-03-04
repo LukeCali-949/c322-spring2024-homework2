19b128f18b4d8a918127e00cc200ab6b3306e73f
LukeCali-949 <mengluke17@gmail.com>
2024-03-03 20:17:13 -0500

    commit v2 for CORS

--- src/main/java/iu/edu/lukemeng/c322spring2024homework2/controllers/HomeController.java
+++ src/main/java/iu/edu/lukemeng/c322spring2024homework2/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package iu.edu.lukemeng.c322spring2024homework2.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class HomeController {
     @GetMapping("/")
     public String greetings(){
