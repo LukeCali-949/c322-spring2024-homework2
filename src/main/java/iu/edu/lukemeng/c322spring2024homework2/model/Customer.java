c1e578118b3960c649ec93d4097a50d1fa2ec842
unknown <fmukona@iu.edu>
2024-04-14 13:23:44 -0400

    login in signin / security

---
+++ src/main/java/iu/edu/lukemeng/c322spring2024homework2/model/Customer.java
@@ -0,0 +1,7 @@
+package iu.edu.lukemeng.c322spring2024homework2.model;
+
+public record Customer(String username,
+                       String password,
+                       String email) {
+
+}
---
+++ src/main/java/iu/edu/lukemeng/c322spring2024homework2/security/UserDetailsSecurityService.java
@@ -0,0 +1,36 @@
+package iu.edu.lukemeng.c322spring2024homework2.security;
+
+
+import iu.edu.lukemeng.c322spring2024homework2.model.Customer;
+import iu.edu.lukemeng.c322spring2024homework2.repository.CustomerRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsSecurityService implements UserDetailsService {
+    CustomerRepository customerRepository;
+
+    public UserDetailsSecurityService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        try {
+            Customer customer = customerRepository.findByUsername(username);
+            if (customer == null) {
+                throw new UsernameNotFoundException("User not found");
+            }
+            return User
+                    .withUsername(username)
+                    .password(customer.password())
+                    .build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
